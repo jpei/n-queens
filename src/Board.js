8456ee8f34c1e39095ad762cd362239133a6d1e6
Joseph Pei <pei_joseph@yahoo.com>
2015-03-11 12:21:08 -0700

    Completed helpers

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,16 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      return _.reduce((this.rows()[rowIndex]), function(a,b) {return a+b;}) > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      for (var i=0; i<this.get('n'); i++) {
+        if (this.hasRowConflictAt(i))
+          return true;
+      }
+      return false;
     },
 
 
@@ -94,12 +98,20 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var count = 0;
+      for (var i=0; i<this.get('n'); i++) {
+        count += this.rows()[i][colIndex];
+      }
+      return count > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      for (var i=0; i<this.get('n'); i++) {
+        if (this.hasColConflictAt(i))
+          return true;
+      }
+      return false;
     },
 
 
@@ -109,12 +121,27 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      if (Math.abs(majorDiagonalColumnIndexAtFirstRow)>=this.get('n'))
+        return false;
+      var count = 0;
+      var steps = 0;
+      while (!this._isInBounds(steps,majorDiagonalColumnIndexAtFirstRow+steps)) {
+        steps++;
+      }
+      while (this._isInBounds(steps, majorDiagonalColumnIndexAtFirstRow+steps)) {
+        count += this.rows()[steps][majorDiagonalColumnIndexAtFirstRow+steps];
+        steps++;
+      }
+      return count > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i=1-this.get('n'); i<this.get('n'); i++) {
+        if (this.hasMajorDiagonalConflictAt(i))
+          return true;
+      }
+      return false;
     },
 
 
@@ -124,12 +151,31 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      if (minorDiagonalColumnIndexAtFirstRow < 0 || minorDiagonalColumnIndexAtFirstRow>2*this.get('n')-2)
+        return false;
+      var count = 0;
+      var steps = 0;
+      while (!this._isInBounds(steps, minorDiagonalColumnIndexAtFirstRow-steps)) {
+        steps++;
+        if (steps>15)
+          debugger;
+      }
+      while (this._isInBounds(steps, minorDiagonalColumnIndexAtFirstRow-steps)) {
+        count += this.rows()[steps][minorDiagonalColumnIndexAtFirstRow-steps];
+        steps++;
+        if (steps>15)
+          debugger;
+      }
+      return count > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i=0; i<=2*this.get('n')-2; i++) {
+        if (this.hasMinorDiagonalConflictAt(i))
+          return true;
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
